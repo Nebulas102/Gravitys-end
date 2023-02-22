2989949f3b38c2bb88d8e059cad1f3eeb136b442
Dustin Timmermans <62051818+DustinTimmermans@users.noreply.github.com>
2023-02-22 22:49:19 +0100

    timer script

---
+++ Assets/Scripts/Core/Timer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float time = 10;
+    public bool timerIsRunning = false;
+    public Text display;
+
+    private void Start()
+    {
+        // Starts the timer
+        timerIsRunning = true;    
+    }
+
+    void Update()
+    {
+        // Checks whether the timer update should be executed
+        if(timerIsRunning)
+        {
+            if (time > 0)
+            {
+                time -= Time.deltaTime;
+                DisplayTime(time);
+            }
+            else
+            {
+                time = 0;
+                Debug.Log("Time has run out!");
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float ttd)
+    {
+        float minutes = Mathf.FloorToInt(ttd / 60);
+        float seconds = Mathf.FloorToInt(ttd % 60);
+
+        display.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}

805fcb5944e7567062c4345fd2438f0a80d9f2a4
Shallow-RB <ryan.bhola2601@gmail.com>
2023-02-23 17:28:15 +0100

    fix: mouse toward camera
    character looks at mouse properly now

--- Assets/Scripts/Controllers/PlayerMovement/LookTowardMouse.cs
+++ Assets/Scripts/Controllers/PlayerMovement/LookTowardMouse.cs
@@ -5,33 +5,53 @@ using UnityEngine;
 public class LookTowardMouse : MonoBehaviour
 {
     [SerializeField]
+    private LayerMask groundMask;
+
+    [SerializeField]
     private new Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        //Get the Screen positions of the object
-        Vector2 positionOnScreen = camera.WorldToViewportPoint(transform.position);
-
-        //Get the Screen position of the mouse
-        Vector2 mouseOnScreen = (Vector2)camera.ScreenToViewportPoint(Input.mousePosition);
-
-        //Get the angle between the points
-        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-        
-        //Ta Daaa
-        transform.rotation = Quaternion.Euler(new Vector3(0f, angle, 0f));
+        Aim();
     }
 
-    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    private void Aim()
     {
-        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+        var (success, position) = GetMousePosition();
+        if (success)
+        {
+            // Calculate the direction
+            var direction = position - transform.position;
+
+            // You might want to delete this line.
+            // Ignore the height difference.
+            direction.y = 0;
+
+            // Make the transform look in the direction.
+            transform.forward = direction;
+        }
     }
+
+    private (bool success, Vector3 position) GetMousePosition()
+    {
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, groundMask))
+        {
+            // The Raycast hit something, return with the position.
+            return (success: true, position: hitInfo.point);
+        }
+        else
+        {
+            // The Raycast did not hit anything.
+            return (success: false, position: Vector3.zero);
+        }
+    }
+
 
 
 }

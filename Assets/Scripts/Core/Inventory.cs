bdd218a9a54f3760c3c099ff232ee956966adf48
Dustin Timmermans <62051818+DustinTimmermans@users.noreply.github.com>
2023-03-12 13:57:37 +0100

    user inventory & moved enemy generation file

---
+++ Assets/Scripts/Core/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Scripts.ScriptableObjects;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Core
+{
+    public class Inventory : MonoBehaviour
+    {
+        public static Inventory Instance;
+        public List<Gear> items = new();
+
+        [SerializeField]
+        private int _itemCap = 6;
+
+        void Awake()
+        {
+            // For better code quality, it's better to use singleton pattern for the inventory
+            if(Instance == null)
+            {
+                Instance = new Inventory();
+            }
+
+            Instance = this;
+        }
+
+        // Add new item to the inventory
+        public bool AddToInventory(Gear item)
+        {
+            if(items.Count >= _itemCap)
+                return false;
+
+            items.Add(item);
+            return true;
+        }
+
+        // Remove item from the inventory
+        public bool RemoveFromInventory(Gear item)
+        {
+            return items.Remove(item);
+        }
+    }
+}

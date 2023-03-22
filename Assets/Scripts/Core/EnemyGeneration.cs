49954f8434a438356ac8e7151112c0fb98aa1089
Bradley Winkel <78041407+Katsurouu@users.noreply.github.com>
2023-03-22 19:02:56 +0100

    Bug testing

--- Assets/Scripts/Core/EnemyGeneration.cs
+++ Assets/Scripts/Core/EnemyGeneration.cs
@@ -7,27 +7,36 @@ namespace Assets.Scripts.Core
     public class EnemyGeneration : MonoBehaviour
     {
         public GameObject enemy;
-        public int xPos;
-        public int zPos;
+        private int xOffset;
+        private int zOffset;
+        public Vector3 spawnpointPos;
         public int enemyCount;
 
         void Start()
         {
+            foreach (Transform child in gameobject.transform) 
+            {
+                if (child.tag == "EnemySpawnpoint")
+                    spawnpointPos = child.transform.position;
+            }
+
+            Debug.Log(spawnpointPos.x);
             StartCoroutine(EnemyDrop());
         }
 
         // Spawns enmies at random x and z coordinates
         IEnumerator EnemyDrop()
         {
-            // Spawns certain amount of enemies, in this case 3
-            while (enemyCount < 3)
+            // Spawns certain amount of enemies, in this case 5 (can also be randomized)
+            while (enemyCount < 5)
             {
                 // Determines where enemies can spawn, will spawn anywhere within the given x and z range
                 // Based on how map generation is handled this will of course change, just hard coded for testing purposes
-                xPos = Random.Range(-6, 6);
-                zPos = Random.Range(1, 8);
+
+                xOffset = Random.Range(-12, 12);
+                zOffset = Random.Range(-12, 12);
                 // Spawns enemy, enemy type can be selected in Unity
-                Instantiate(enemy, new Vector3(xPos, 0, zPos), Quaternion.identity);
+                Instantiate(enemy, new Vector3(spawnpointPos.x-xOffset, 0, spawnpointPos.z-zOffset), Quaternion.identity);
                 // Waits a bit before spawning next enemy
                 yield return new WaitForSeconds(0.1f);
                 enemyCount += 1;

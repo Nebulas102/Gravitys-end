0cd7d5791b1918abc4420b421042bab8f36ad712
Dustin Timmermans <62051818+DustinTimmermans@users.noreply.github.com>
2023-03-28 21:46:22 +0200

    Update Item.cs

--- Assets/Scripts/ScriptableObjects/Item.cs
+++ Assets/Scripts/ScriptableObjects/Item.cs
@@ -1,3 +1,4 @@
+using System;
 using Core.Inventory;
 using UnityEngine;
 
@@ -7,79 +8,87 @@ namespace ScriptableObjects
     public class Item : ScriptableObject
     {
         public string Name;
-        public Sprite icon = null;
-        public bool isDefaultItem = false;
-        [SerializeField] public Type type;
+        public Sprite icon;
+        public bool isDefaultItem;
+
+        [SerializeField]
+        public Type type;
 
         public virtual void Use(InventorySlot itemSlot)
         {
-            if (this.type == Type.ARMOR) {
-
-                if (itemSlot.isCurrentSlot) {
+            switch (type)
+            {
+                case Type.ARMOR when itemSlot.isCurrentSlot:
+                {
                     // DISCONTINUED cant figure this one out so the best solution is to use the remove button in the inventory
 
                     // Check if other (non current slots) are available to put this item in
-                    // If so remove everything from the currentslot and put it in the available one
+                    // If so remove everything from the current slot and put it in the available one
                     // Else dont do anything
-                    if (InventoryUI.instance.CheckForAvailableSlots(Type.ARMOR) != null) {
-                        InventoryUI.instance.CheckForAvailableSlots(Type.ARMOR).AddItem(InventoryUI.staticCurrentArmorSlot.GetItem());
-                        Inventory.instance.Add(InventoryUI.staticCurrentArmorSlot.GetItem());
-                        InventoryUI.staticCurrentArmorSlot.ClearSlot();
-                    }
+                    if (InventoryUI.instance.CheckForAvailableSlots(Type.ARMOR) == null) return;
+
+                    InventoryUI.instance.CheckForAvailableSlots(Type.ARMOR)
+                        .AddItem(InventoryUI.staticCurrentArmorSlot.GetItem());
+                    Inventory.instance.Add(InventoryUI.staticCurrentArmorSlot.GetItem());
+                    InventoryUI.staticCurrentArmorSlot.ClearSlot();
                     return;
                 }
-
                 // Remove from the current slot; 
-                itemSlot.ClearSlot();
-                Inventory.instance.Remove(this);
+                case Type.ARMOR:
+                {
+                    itemSlot.ClearSlot();
+                    Inventory.instance.Remove(this);
 
-                // Check if the current slot has an item, if so, put the item of the current slot in this slot
-                // and put this item in the current slot (Swapping the items.)
-                if (InventoryUI.staticCurrentArmorSlot.GetItem() != null) {
+                    // Check if the current slot has an item, if so, put the item of the current slot in this slot
+                    // and put this item in the current slot (Swapping the items.)
+                    if (InventoryUI.staticCurrentArmorSlot.GetItem() == null) return;
+
                     itemSlot.AddItem(InventoryUI.staticCurrentArmorSlot.GetItem());
                     Inventory.instance.Add(InventoryUI.staticCurrentArmorSlot.GetItem());
                     InventoryUI.staticCurrentArmorSlot.ClearSlot();
+
+                    // Add this item to the currentSlot
+                    InventoryUI.staticCurrentArmorSlot.AddItem(this);
+                    break;
                 }
-
-                // Add this item to the currentSlot
-                InventoryUI.staticCurrentArmorSlot.AddItem(this);
-            }
-
-            if (this.type == Type.WEAPON) {
-                
-                if (itemSlot.isCurrentSlot) {
+                case Type.WEAPON when itemSlot.isCurrentSlot:
+                {
                     // DISCONTINUED cant figure this one out so the best solution is to use the remove button in the inventory
 
                     // Check if other (non current slots) are available to put this item in
-                    // If so remove everything from the currentslot and put it in the available one
+                    // If so remove everything from the current slot and put it in the available one
                     // Else dont do anything
-                    if (InventoryUI.instance.CheckForAvailableSlots(Type.WEAPON) != null) {
-                        InventoryUI.instance.CheckForAvailableSlots(Type.WEAPON).AddItem(InventoryUI.staticCurrentWeaponSlot.GetItem());
-                        Inventory.instance.Add(InventoryUI.staticCurrentWeaponSlot.GetItem());
-                        InventoryUI.staticCurrentWeaponSlot.ClearSlot();
-                    }
+                    if (InventoryUI.instance.CheckForAvailableSlots(Type.WEAPON) == null) return;
+
+                    InventoryUI.instance.CheckForAvailableSlots(Type.WEAPON)
+                        .AddItem(InventoryUI.staticCurrentWeaponSlot.GetItem());
+                    Inventory.instance.Add(InventoryUI.staticCurrentWeaponSlot.GetItem());
+                    InventoryUI.staticCurrentWeaponSlot.ClearSlot();
+
                     return;
                 }
-
                 // Remove from the current slot; 
-                itemSlot.ClearSlot();
-                Inventory.instance.Remove(this);
+                case Type.WEAPON:
+                {
+                    itemSlot.ClearSlot();
+                    Inventory.instance.Remove(this);
 
-                // Check if the current slot has an item, if so, put the item of the current slot in this slot
-                // and put this item in the current slot (Swapping the items.)
-                if (InventoryUI.staticCurrentWeaponSlot.GetItem() != null) {
+                    // Check if the current slot has an item, if so, put the item of the current slot in this slot
+                    // and put this item in the current slot (Swapping the items.)
+                    if (InventoryUI.staticCurrentWeaponSlot.GetItem() == null) return;
+
                     itemSlot.AddItem(InventoryUI.staticCurrentWeaponSlot.GetItem());
                     Inventory.instance.Add(InventoryUI.staticCurrentWeaponSlot.GetItem());
                     InventoryUI.staticCurrentWeaponSlot.ClearSlot();
+
+                    // Add this item to the currentSlot
+                    InventoryUI.staticCurrentWeaponSlot.AddItem(this);
+                    break;
                 }
-
-                // Add this item to the currentSlot
-                InventoryUI.staticCurrentWeaponSlot.AddItem(this);
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
-
         }
-
-
     }
 
     public enum Type
@@ -87,7 +96,4 @@ namespace ScriptableObjects
         WEAPON,
         ARMOR
     }
-
 }
-
-

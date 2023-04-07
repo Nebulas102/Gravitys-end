b0b5b248acf57824f5f9f61141e4103b0e3403b4
Mike <mjb102denhaag@gmail.com>
2023-04-07 16:23:50 +0200

    Fixed stage navmesh baker
    it only looks now at the gameobject
    with the tags:
    door
    wall
    floor

--- Assets/Scripts/Core/EnemyGeneration.cs
+++ Assets/Scripts/Core/EnemyGeneration.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -44,12 +46,57 @@ namespace Assets.Scripts.Core
 
         IEnumerator NavMeshBaker()
         {
-            NavMeshSurface[] navMeshSurfaces = FindObjectsOfType<NavMeshSurface>();
-            for(int i = 0; i < navMeshSurfaces.Length; i++) 
+            // Find all game objects with the specified tag
+            GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag("Room");
+
+            // Create a NavMeshBuildSettings object
+            NavMeshBuildSettings settings = NavMesh.GetSettingsByID(0);
+
+            // Create an array to hold the NavMeshBuildSources
+            NavMeshBuildSource[] sources = new NavMeshBuildSource[0];
+
+            // Iterate through all the tagged game objects and their children
+            foreach (GameObject obj in taggedObjects)
             {
-                navMeshSurfaces[i].BuildNavMesh();
+                AddSourcesFromObject(obj, ref sources);
             }
-            yield return new WaitForSeconds(0.1f);
+
+            // Build the NavMesh
+            NavMeshData data = new NavMeshData();
+            data = NavMeshBuilder.BuildNavMeshData(settings, sources.ToList(), new Bounds(Vector3.zero, new Vector3(1000, 20, 1000)), Vector3.zero, Quaternion.identity);
+            NavMesh.AddNavMeshData(data);
+            Debug.Log("NavMesh baked successfully");
+
+            yield return null;
+        }
+
+        private void AddSourcesFromObject(GameObject obj, ref NavMeshBuildSource[] sources)
+        {
+            MeshFilter[] meshFilters = obj.GetComponentsInChildren<MeshFilter>();
+
+            // Add a NavMeshBuildSource for each mesh filter
+            foreach (MeshFilter filter in meshFilters)
+            {
+                if (obj.tag == "Floor" || obj.tag == "Wall" || obj.tag == "Door")
+                {
+                    NavMeshBuildSource source = new NavMeshBuildSource()
+                    {
+                        transform = filter.transform.localToWorldMatrix,
+                        shape = NavMeshBuildSourceShape.Mesh,
+                        sourceObject = filter.sharedMesh,
+                        area = 0
+                    };
+
+                    // Add the NavMeshBuildSource to the sources array
+                    ArrayUtility.Add(ref sources, source);
+                }
+            }
+
+            // Recursively add sources from all children
+            foreach (Transform child in obj.transform)
+            {
+                AddSourcesFromObject(child.gameObject, ref sources);
+            }
         }
     }
 }

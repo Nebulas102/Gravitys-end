f0d469611edf3c68b405ac38b26472f3ded5579d
Dustin Timmermans <62051818+DustinTimmermans@users.noreply.github.com>
2023-04-11 16:41:50 +0200

    .

---
+++ Assets/Scripts/Core/LootGeneration.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    public class LootGeneration : MonoBehaviour
+    {
+        [SerializeField]
+        private List<GameObject> lootObjects;
+    }
+}

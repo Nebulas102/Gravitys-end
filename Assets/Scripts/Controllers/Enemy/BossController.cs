b2393358455e4494ecceabf463535ee88e4da8be
Mike <mjb102denhaag@gmail.com>
2023-04-13 16:41:06 +0200

    Fixed merge conflicts + refactor

--- Assets/Scripts/Controllers/Enemy/BossController.cs
+++ Assets/Scripts/Controllers/Enemy/BossController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using StageGeneration.Rooms.RoomTypes;
 
 public class BossController : MonoBehaviour
 {

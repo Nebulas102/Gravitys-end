5eb9f56f14c577a8d6eead4d403b3caf55b0fad4
Mike <mjb102denhaag@gmail.com>
2023-04-21 21:29:19 +0200

    new stage gen

--- Assets/Scripts/StageGeneration/Rooms/RoomTypes/SpawnRoom.cs
+++ Assets/Scripts/StageGeneration/Rooms/RoomTypes/SpawnRoom.cs
@@ -1,6 +1,10 @@
+using UnityEngine;
+
 namespace StageGeneration.Rooms.RoomTypes
 {
     public class SpawnRoom : Room
     {
+        public GameObject bossDoor;
+        public GameObject bossRoomHallway;
     }
 }

3a8f7dc77da3c27c2bf7dba5338397ea2bd07033
Bradley Winkel <78041407+Katsurouu@users.noreply.github.com>
2023-05-09 17:23:57 +0200

    Groups added
    Enemies now spawn in groups

--- Assets/Scripts/Core/EnemyGeneration.cs
+++ Assets/Scripts/Core/EnemyGeneration.cs
@@ -12,25 +12,31 @@ namespace Core
         public GameObject enemy;
         [SerializeField]
         private Transform[] enemySpawnPoints;
+        private Vector3 spawnPosition;
+        private int groupSize;
+        [SerializeField]
+        private int minGroupSize;
+        [SerializeField]
+        private int maxGroupSize;
         
 
         void Start()
         {
-            // Create walkable surface for enemies
-            // StartCoroutine(NavMeshBaker());
-            // Spawn enemies
-            
-            // Debug.Log(spawnpointPos.x);
-            // StartCoroutine(EnemyDrop());
+
         }
 
-        // Spawns enemies at random x and z coordinates
         IEnumerator EnemyDrop()
         {
             for (int i = 0; i < enemySpawnPoints.Length; i++)
             {
-                Instantiate(enemy, enemySpawnPoints[i].position, Quaternion.identity);
-                yield return new WaitForSeconds(0.1f);
+                groupSize = Random.Range(minGroupSize, maxGroupSize + 1);
+                for (int _ = 0; _ < groupSize; _++)
+                {
+                    spawnPosition = enemySpawnPoints[i].position;
+                    spawnPosition.x += Random.Range(-1f, 1f); // Slightly randomize the X position so enemies dont collide upon spawning
+                    Instantiate(enemy, spawnPosition, Quaternion.identity);
+                    yield return new WaitForSeconds(0.1f);
+                }
             }
         }
 
@@ -39,59 +45,5 @@ namespace Core
             StartCoroutine(EnemyDrop());
         }
 
-        // IEnumerator NavMeshBaker()
-        // {
-        //     // Find all game objects with the specified tag
-        //     GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag("Room");
-
-        //     // Create a NavMeshBuildSettings object
-        //     NavMeshBuildSettings settings = NavMesh.GetSettingsByID(0);
-
-        //     // Create an array to hold the NavMeshBuildSources
-        //     NavMeshBuildSource[] sources = new NavMeshBuildSource[0];
-
-        //     // Iterate through all the tagged game objects and their children
-        //     foreach (GameObject obj in taggedObjects)
-        //     {
-        //         AddSourcesFromObject(obj, ref sources);
-        //     }
-
-        //     // Build the NavMesh
-        //     NavMeshData data = new NavMeshData();
-        //     data = NavMeshBuilder.BuildNavMeshData(settings, sources.ToList(), new Bounds(Vector3.zero, new Vector3(1000, 20, 1000)), Vector3.zero, Quaternion.identity);
-        //     NavMesh.AddNavMeshData(data);
-        //     Debug.Log("NavMesh baked successfully");
-
-        //     yield return null;
-        // }
-
-        // private void AddSourcesFromObject(GameObject obj, ref NavMeshBuildSource[] sources)
-        // {
-        //     MeshFilter[] meshFilters = obj.GetComponentsInChildren<MeshFilter>();
-
-        //     // Add a NavMeshBuildSource for each mesh filter
-        //     foreach (MeshFilter filter in meshFilters)
-        //     {
-        //         if (obj.tag == "Floor" || obj.tag == "Wall" || obj.tag == "Door")
-        //         {
-        //             NavMeshBuildSource source = new NavMeshBuildSource()
-        //             {
-        //                 transform = filter.transform.localToWorldMatrix,
-        //                 shape = NavMeshBuildSourceShape.Mesh,
-        //                 sourceObject = filter.sharedMesh,
-        //                 area = 0
-        //             };
-
-        //             // Add the NavMeshBuildSource to the sources array
-        //             ArrayUtility.Add(ref sources, source);
-        //         }
-        //     }
-
-        //     // Recursively add sources from all children
-        //     foreach (Transform child in obj.transform)
-        //     {
-        //         AddSourcesFromObject(child.gameObject, ref sources);
-        //     }
-        // }
     }
 }

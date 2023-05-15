2af51f52b7556ccbee0f6a2cadfdcf5d6b18020d
Bradley Winkel <78041407+Katsurouu@users.noreply.github.com>
2023-05-15 16:09:29 +0200

    Randomly chooses spawnpoints

--- Assets/Scripts/Core/EnemyGeneration.cs
+++ Assets/Scripts/Core/EnemyGeneration.cs
@@ -27,18 +27,27 @@ namespace Core
 
         IEnumerator EnemyDrop()
         {
-            for (int i = 0; i < enemySpawnPoints.Length; i++)
+            List<Transform> availableSpawnPoints = new List<Transform>(enemySpawnPoints);
+            int spawnPointCount = availableSpawnPoints.Count;
+
+            for (int i = 0; i < 3 && spawnPointCount > 0; i++)
             {
+                int randomIndex = Random.Range(0, spawnPointCount);
                 groupSize = Random.Range(minGroupSize, maxGroupSize + 1);
+                spawnPosition = availableSpawnPoints[randomIndex].position;
+
                 for (int _ = 0; _ < groupSize; _++)
                 {
-                    spawnPosition = enemySpawnPoints[i].position;
-                    spawnPosition.x += Random.Range(-1f, 1f); // Slightly randomize the X position so enemies dont collide upon spawning
+                    spawnPosition.x += Random.Range(-1f, 1f);
                     Instantiate(enemy, spawnPosition, Quaternion.identity);
                     yield return new WaitForSeconds(0.1f);
                 }
+
+                availableSpawnPoints.RemoveAt(randomIndex);
+                spawnPointCount--;
             }
         }
+
 
         public void SpawnEnemy()
         {

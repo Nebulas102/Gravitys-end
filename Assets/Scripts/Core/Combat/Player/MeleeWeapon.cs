32353f727eac36349de70c9f22797e534b368379
Mike <mjb102denhaag@gmail.com>
2023-05-17 20:20:55 +0200

    added new sword

--- Assets/Scripts/Core/Combat/Player/MeleeWeapon.cs
+++ Assets/Scripts/Core/Combat/Player/MeleeWeapon.cs
@@ -14,6 +14,7 @@ public class MeleeWeapon : MonoBehaviour
     private bool isEquipped = false;
     private Collider hitbox;
 
+    [HideInInspector]
     public bool allowAttack = false;
 
     private void Start()

fc12b2e0dc0d7e6f152eaccab07986c6544c116a
Dustin Timmermans <62051818+DustinTimmermans@users.noreply.github.com>
2023-06-22 01:53:06 +0200

    Update UIHandler.cs

--- Assets/Scripts/UI/UIHandler.cs
+++ Assets/Scripts/UI/UIHandler.cs
@@ -46,31 +46,6 @@ namespace UI
             OnPauseGameToggle.Invoke(condition);
         }
 
-        public void ShowPickupPrompt(ItemType type, bool show)
-        {
-            if (!show)
-            {
-                itemPickupPrompt.SetActive(false);
-                return;
-            }
-
-            var prompt = itemPickupPrompt.GetComponent<TextMeshProUGUI>();
-            if (InventoryManager.instance.IsInventoryFull(type))
-                prompt.text = "Inventory full";
-            else
-            {
-                var action = _inputManager.Player.LootPickup;
-                int bindingIndex = action.GetBindingIndexForControl(action.controls[0]);
-                string key = InputControlPath.ToHumanReadableString(
-                    action.bindings[bindingIndex].effectivePath,
-                    InputControlPath.HumanReadableStringOptions.OmitDevice
-                );
-                prompt.text = $"[{key}] Take";
-            }
-
-            itemPickupPrompt.SetActive(true);
-        }
-
         private void OnShowPickupPrompt(bool show, ItemType type)
         {
             if (!show)

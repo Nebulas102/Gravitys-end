b1908bed78d778fc858afb574b8c9ea00a9295db
Mike <mjb102denhaag@gmail.com>
2023-06-24 14:54:07 +0200

    added particle helper

--- Assets/Scripts/Core/Combat/Player/Effects/PlayerWeaponSlash.cs
+++ Assets/Scripts/Core/Combat/Player/Effects/PlayerWeaponSlash.cs
@@ -20,88 +20,35 @@ public class PlayerWeaponSlash : MonoBehaviour
     {
         for (int i = 0; i < attacks.Length; i++)
         {
-            ChangeShaderColor(i, "_AddColor", slash);
-            ChangeShaderTexture(i, "_EmissionTex", slashTexture);
+            var parentParticle = attacks[i].transform;
+
+            ParticleHelper.ChangeShaderColor(parentParticle, "_AddColor", slash);
+            ParticleHelper.ChangeShaderTexture(parentParticle, "_EmissionTex", slashTexture);
 
             if (i > 2)
             {
-                ChangeShaderColor(i, "Slash2", "_AddColor", slash);
-                ChangeShaderTexture(i, "Slash2", "_EmissionTex", slashTexture);
+                ParticleHelper.ChangeShaderColor(parentParticle, "Slash2", "_AddColor", slash);
+                ParticleHelper.ChangeShaderTexture(parentParticle, "Slash2", "_EmissionTex", slashTexture);
 
-                ChangeShaderColor(i, "Slash3", "_AddColor", slash);
-                ChangeShaderTexture(i, "Slash3", "_EmissionTex", slashTexture);
+                ParticleHelper.ChangeShaderColor(parentParticle, "Slash3", "_AddColor", slash);
+                ParticleHelper.ChangeShaderTexture(parentParticle, "Slash3", "_EmissionTex", slashTexture);
 
-                ChangeParticleColor(i, "Fire", fire);
+                ParticleHelper.ChangeParticleColor(parentParticle, "Fire", fire);
             }
             else
             {
-                ChangeParticleColor(i, "Smoke", smoke);
+                ParticleHelper.ChangeParticleColor(parentParticle, "Smoke", smoke);
             }
 
-            ChangeParticleColor(i, "Sparks", spark);
-            ChangeParticleColor(i, "Hit", hit);
+            ParticleHelper.ChangeParticleColor(parentParticle, "Sparks", spark);
+            ParticleHelper.ChangeParticleColor(parentParticle, "Hit", hit);
 
-            var attackHit = attacks[i].transform.Find("Hit");
-            attackHit.GetComponent<ParticleSystemRenderer>().material.SetColor("_Color", hit);
+            var attackHit = parentParticle.Find("Hit");
 
-            ChangeParticleColor("Flare", sparksCore, attackHit);
-            ChangeParticleColor("SparksCore", sparksCore, attackHit);
+            ParticleHelper.ChangeShaderColor(attackHit, "_Color", hit);
+
+            ParticleHelper.ChangeParticleColor(attackHit, "Flare", sparksCore);
+            ParticleHelper.ChangeParticleColor(attackHit, "SparksCore", sparksCore);
         }
-    }
-
-    private void ChangeShaderTexture(int id, string nameField, Texture texture)
-    {
-        var particleRenderer = attacks[id].GetComponent<ParticleSystemRenderer>();
-        var originalMat = particleRenderer.material;
-        var newMat = new Material(originalMat);
-
-        newMat.SetTexture(nameField, texture);
-
-        particleRenderer.material = newMat;
-    }
-
-    private void ChangeShaderTexture(int id, string name, string nameField, Texture texture)
-    {
-        var particleRenderer = attacks[id].transform.Find(name).GetComponent<ParticleSystemRenderer>();
-        var originalMat = particleRenderer.material;
-        var newMat = new Material(originalMat);
-
-        newMat.SetTexture(nameField, texture);
-
-        particleRenderer.material = newMat;
-    }
-
-    private void ChangeShaderColor(int id, string nameField, Color color)
-    {
-        var particleRenderer = attacks[id].GetComponent<ParticleSystemRenderer>();
-        var originalMat = particleRenderer.material;
-        var newMat = new Material(originalMat);
-
-        newMat.SetColor(nameField, color);
-
-        particleRenderer.material = newMat;
-    }
-
-    private void ChangeShaderColor(int id, string name, string nameField, Color color)
-    {
-        var particleRenderer = attacks[id].transform.Find(name).GetComponent<ParticleSystemRenderer>();
-        var originalMat = particleRenderer.material;
-        var newMat = new Material(originalMat);
-
-        newMat.SetColor(nameField, color);
-
-        particleRenderer.material = newMat;
-    }
-
-    private void ChangeParticleColor(int id, string name, Color color)
-    {
-        var mainModuleSparks = attacks[id].transform.Find(name).GetComponent<ParticleSystem>().main;
-        mainModuleSparks.startColor = color;
-    }
-
-    private void ChangeParticleColor(string name, Color color, Transform particleObject)
-    {
-        var mainModuleSparks = particleObject.Find(name).GetComponent<ParticleSystem>().main;
-        mainModuleSparks.startColor = color;
     }
 }
